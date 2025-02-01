53181da396fdd75dfaa835e5501b4d33d2fc065d
flvsantos15 <flvsantos300@gmail.com>
2025-01-31 22:10:23 -0300

    chore: add search params and search result

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -12,3 +12,13 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
 
   getEntity(): new (...args: any[]) => E
 }
+
+export interface ISearchableRepositorynds<
+  E extends Entity,
+  EntityId extends ValueObject,
+  SearchInput,
+  SearchOutput
+> extends IRepository<E, EntityId> {
+  sortableFields: string[]
+  search(props: SearchInput): Promise<SearchOutput>
+}

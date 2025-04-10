c6fe67917958921c0f605501b128d9457e9a637d
flvsantos15 <flvsantos300@gmail.com>
2025-04-10 08:41:03 -0300

    feat: add category use case

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,85 +1,83 @@
-import { EntityValidationError } from '../../shared/domain/validators/validation.error'
-import ValidatorRules from '../../shared/domain/validators/validator-rule'
-import { Uuid } from '../../shared/domain/value-objects/uuid.vo'
-import {
-  CastegoryValidator,
-  CategoryValidatorFactory
-} from './category.validator'
-import { Entity } from '../../shared/domain/entity'
-import { ValueObject } from '../../shared/domain/value-object'
-import { CategoryFakeBuilder } from './category-fake.builder'
+import { Entity } from "../../shared/domain/entity";
+import { EntityValidationError } from "../../shared/domain/validators/validation.error";
+import { ValueObject } from "../../shared/domain/value-object";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryFakeBuilder } from "./category-fake.builder";
+import { CategoryValidatorFactory } from "./category.validator";
 
 export type CategoryProps = {
-  category_id?: Uuid
-  name: string
-  description?: string | null
-  is_active?: boolean
-  created_at?: Date
-}
+  category_id?: Uuid;
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+  created_at?: Date;
+};
 
 export type CategoryCreateCommand = {
-  name: string
-  description?: string | null
-  is_active?: boolean
-}
+  name: string;
+  description?: string | null;
+  is_active?: boolean;
+};
+
+export class CategoryId extends Uuid {}
 
 export class Category extends Entity {
-  category_id: Uuid
-  name: string
-  description: string | null
-  is_active: boolean
-  created_at: Date
+  category_id: Uuid;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
 
   constructor(props: CategoryProps) {
-    super()
-    this.category_id = props.category_id ?? new Uuid()
-    this.name = props.name
-    this.description = props.description ?? null
-    this.is_active = props.is_active ?? true
-    this.created_at = props.created_at ?? new Date()
+    super();
+    this.category_id = props.category_id ?? new Uuid();
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
   }
 
   get entity_id(): ValueObject {
-    return this.category_id
+    return this.category_id;
   }
 
   // factory method
   static create(props: CategoryCreateCommand): Category {
-    const category = new Category(props)
-    Category.validate(category)
+    const category = new Category(props);
+    Category.validate(category);
 
-    return new Category(props)
+    return new Category(props);
   }
 
   changeName(name: string): void {
-    this.name = name
-    Category.validate(this)
+    this.name = name;
+    Category.validate(this);
   }
 
   changeDescription(description: string): void {
-    this.description = description
-    Category.validate(this)
+    this.description = description;
+    Category.validate(this);
   }
 
   activate() {
-    this.is_active = true
+    this.is_active = true;
   }
 
   deactivate() {
-    this.is_active = false
+    this.is_active = false;
   }
 
   static validate(entity: Category) {
-    const validator = CategoryValidatorFactory.create()
-    const isValid = validator.validate(entity)
+    const validator = CategoryValidatorFactory.create();
+    const isValid = validator.validate(entity);
 
     if (!isValid) {
-      throw new EntityValidationError(validator.errors)
+      throw new EntityValidationError(validator.errors);
     }
   }
 
   static fake() {
-    return CategoryFakeBuilder
+    return CategoryFakeBuilder;
   }
 
   toJSON() {
@@ -88,7 +86,7 @@ export class Category extends Entity {
       name: this.name,
       description: this.description,
       is_active: this.is_active,
-      created_at: this.created_at
-    }
+      created_at: this.created_at,
+    };
   }
 }
